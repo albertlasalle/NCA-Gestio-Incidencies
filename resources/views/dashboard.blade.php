9a3f762b101973e2abb7aed89a25e50cce960a3e
albertmollerussa <albertdalmaubravo@mollerussa.lasalle.cat>
2024-02-08 09:52:31 +0100

    ajustar login amb autenticació

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,18 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{ __("Login Correcte!") }}
+
                 </div>
+
+                
             </div>
+
+            <a href="{{ route('admin/incidencias') }}" class="btn btn-success mt-4 ml-3"> Incidencias </a>
+
+                <a href="{{ route('admin/empresas') }}" class="btn btn-success mt-4 ml-3"> Empresas </a>
         </div>
     </div>
+
+    
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -27,74 +27,77 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    /* Productos */
+
+    /* Crear */
+    Route::get('admin/productos/crear', 'App\Http\Controllers\ProductosController@crear')->name('admin/productos/crear');
+    Route::put('admin/productos/store', 'App\Http\Controllers\ProductosController@store')->name('admin/productos/store');
+    
+    /* Leer */ 
+    Route::get('admin/productos/show/{id}', 'App\Http\Controllers\ProductosController@show')->name('admin/productos/detalles'); 
+    
+    /* Actualizar */
+    Route::get('admin/productos/actualizar/{id}', 'App\Http\Controllers\ProductosController@actualizar')->name('admin/productos/actualizar');
+    Route::put('admin/productos/update/{id}', 'App\Http\Controllers\ProductosController@update')->name('admin/productos/update');
+    
+    /* Eliminar */
+    Route::put('admin/productos/eliminar/{id}', 'App\Http\Controllers\ProductosController@eliminar')->name('admin/productos/eliminar'); 
+    
+    /* Vista Principal */
+    
+    Route::post('admin/productos', 'App\Http\Controllers\ProductosController@index')->name('admin/productos');
+    Route::get('admin/productos', 'App\Http\Controllers\ProductosController@index')->name('admin/productos');
+    
+    
+    /* Incidencias */
+    
+    /* Crear */
+    Route::get('admin/incidencias/crear', 'App\Http\Controllers\IncidenciasController@crear')->name('admin/incidencias/crear'); 
+    Route::put('admin/incidencias/store', 'App\Http\Controllers\IncidenciasController@store')->name('admin/incidencias/store'); 
+    
+    /* Leer */ 
+    Route::get('admin/incidencias/show/{id}', 'App\Http\Controllers\IncidenciasController@show')->name('admin/incidencias/detalles'); 
+    
+    /* Actualizar */
+    Route::get('admin/incidencias/actualizar/{id}', 'App\Http\Controllers\IncidenciasController@actualizar')->name('admin/incidencias/actualizar'); 
+    Route::put('admin/incidencias/update/{id}', 'App\Http\Controllers\IncidenciasController@update')->name('admin/incidencias/update'); 
+    
+    /* Eliminar */
+    Route::put('admin/incidencias/eliminar/{id}', 'App\Http\Controllers\IncidenciasController@eliminar')->name('admin/incidencias/eliminar'); 
+    
+    /* Vista Principal */
+    Route::get('admin/incidencias', 'App\Http\Controllers\IncidenciasController@index')->name('admin/incidencias'); 
+    Route::post('admin/incidencias', 'App\Http\Controllers\IncidenciasController@index')->name('admin/incidencias'); 
+    
+    /* Empresas */
+    
+    /* Crear */
+    
+    Route::get('admin/empresas/crear', 'App\Http\Controllers\EmpresasController@crear')->name('admin/empresas/crear');
+    Route::put('admin/empresas/store', 'App\Http\Controllers\EmpresasController@store')->name('admin/empresas/store');
+    
+    /* Leer */
+    
+    Route::get('admin/empresas/show/{id}', 'App\Http\Controllers\EmpresasController@show')->name('admin/empresas/detalles');
+    
+    /* Actualizar */
+    
+    Route::get('admin/empresas/actualizar/{id}', 'App\Http\Controllers\EmpresasController@actualizar')->name('admin/empresas/actualizar');
+    Route::put('admin/empresas/update/{id}', 'App\Http\Controllers\EmpresasController@update')->name('admin/empresas/update');
+    
+    /* Eliminar */
+    
+    Route::put('admin/empresas/eliminar/{id}', 'App\Http\Controllers\EmpresasController@eliminar')->name('admin/empresas/eliminar');
+    
+    /* Vista Principal */
+    
+    Route::get('admin/empresas', 'App\Http\Controllers\EmpresasController@index')->name('admin/empresas');
+    Route::post('admin/empresas', 'App\Http\Controllers\EmpresasController@index')->name('admin/empresas');
+
+
 });
 
-/* Crear */
-Route::get('admin/productos/crear', 'App\Http\Controllers\ProductosController@crear')->name('admin/productos/crear');
-Route::put('admin/productos/store', 'App\Http\Controllers\ProductosController@store')->name('admin/productos/store');
- 
-/* Leer */ 
-Route::get('admin/productos/show/{id}', 'App\Http\Controllers\ProductosController@show')->name('admin/productos/detalles'); 
- 
-/* Actualizar */
-Route::get('admin/productos/actualizar/{id}', 'App\Http\Controllers\ProductosController@actualizar')->name('admin/productos/actualizar');
-Route::put('admin/productos/update/{id}', 'App\Http\Controllers\ProductosController@update')->name('admin/productos/update');
- 
-/* Eliminar */
-Route::put('admin/productos/eliminar/{id}', 'App\Http\Controllers\ProductosController@eliminar')->name('admin/productos/eliminar'); 
- 
-/* Vista Principal */
 
-Route::post('admin/productos', 'App\Http\Controllers\ProductosController@index')->name('admin/productos');
-
-Route::get('admin/productos', 'App\Http\Controllers\ProductosController@index')->name('admin/productos');
-
-
-
-/* Incidencias */
-
-/* Crear */
-Route::get('admin/incidencias/crear', 'App\Http\Controllers\IncidenciasController@crear')->name('admin/incidencias/crear'); 
-Route::put('admin/incidencias/store', 'App\Http\Controllers\IncidenciasController@store')->name('admin/incidencias/store'); 
- 
-/* Leer */ 
-Route::get('admin/incidencias/show/{id}', 'App\Http\Controllers\IncidenciasController@show')->name('admin/incidencias/detalles'); 
- 
-/* Actualizar */
-Route::get('admin/incidencias/actualizar/{id}', 'App\Http\Controllers\IncidenciasController@actualizar')->name('admin/incidencias/actualizar'); 
-Route::put('admin/incidencias/update/{id}', 'App\Http\Controllers\IncidenciasController@update')->name('admin/incidencias/update'); 
- 
-/* Eliminar */
-Route::put('admin/incidencias/eliminar/{id}', 'App\Http\Controllers\IncidenciasController@eliminar')->name('admin/incidencias/eliminar'); 
- 
-/* Vista Principal */
-Route::get('admin/incidencias', 'App\Http\Controllers\IncidenciasController@index')->name('admin/incidencias'); 
-Route::post('admin/incidencias', 'App\Http\Controllers\IncidenciasController@index')->name('admin/incidencias'); 
-
-/* Empresas */
-
-/* Crear */
-
-Route::get('admin/empresas/crear', 'App\Http\Controllers\EmpresasController@crear')->name('admin/empresas/crear');
-Route::put('admin/empresas/store', 'App\Http\Controllers\EmpresasController@store')->name('admin/empresas/store');
-
-/* Leer */
-
-Route::get('admin/empresas/show/{id}', 'App\Http\Controllers\EmpresasController@show')->name('admin/empresas/detalles');
-
-/* Actualizar */
-
-Route::get('admin/empresas/actualizar/{id}', 'App\Http\Controllers\EmpresasController@actualizar')->name('admin/empresas/actualizar');
-Route::put('admin/empresas/update/{id}', 'App\Http\Controllers\EmpresasController@update')->name('admin/empresas/update');
-
-/* Eliminar */
-
-Route::put('admin/empresas/eliminar/{id}', 'App\Http\Controllers\EmpresasController@eliminar')->name('admin/empresas/eliminar');
-
-/* Vista Principal */
-
-Route::get('admin/empresas', 'App\Http\Controllers\EmpresasController@index')->name('admin/empresas');
-Route::post('admin/empresas', 'App\Http\Controllers\EmpresasController@index')->name('admin/empresas');
 
 
 require __DIR__.'/auth.php';
